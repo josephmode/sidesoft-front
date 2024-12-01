22d40d754d9f9237fed4ed74452abebfbb2ca708
josephmode <jjddvv10@gmail.com>
2024-12-01 10:50:17 -0500

    pueto eliminado

--- src/Components/CoinMarketComponent/CoinMarket.jsx
+++ src/Components/CoinMarketComponent/CoinMarket.jsx
@@ -8,7 +8,7 @@ export const CoinMarket = (req, res) => {
 
 
     const getData = async () => {
-        const url = "https://sidesoft-test.ddns.net:3001/";
+        const url = "https://sidesoft-test.ddns.net/";
         try {
             const respuesta = await fetch(url);
             if (!respuesta.ok) {
